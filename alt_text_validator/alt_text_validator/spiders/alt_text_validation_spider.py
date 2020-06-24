f5337fbb32c2127ab2be24744affc127f7b80454
harootune <56971958+harootune@users.noreply.github.com>
2020-06-23 23:31:44 -0500

    Domain filtering Additions: + Added domain filtering to avoid crawling domains beyond library.illinois.edu Wordpress sites, which leads to unexpected img tag extraction behavior
    TODO:
    * Return to Helen and decide where to go from here
    * Better CSV handling

--- alt_text_validator/alt_text_validator/spiders/alt_text_validation_spider.py
+++ alt_text_validator/alt_text_validator/spiders/alt_text_validation_spider.py
@@ -13,7 +13,8 @@ class ATVSpider(CrawlSpider):
     name = 'atv-spider'
     rules = (
         Rule(LinkExtractor(restrict_xpaths=('//*[@id="bs-navbar-header"]',),
-                           deny='.*#.*'), follow=True, callback='parse_missing_alt_text'),
+                           allow_domains='library.illinois.edu', deny='.*#.*'),
+             follow=True, callback='parse_missing_alt_text'),
     )
     custom_settings = {
          'ITEM_PIPELINES': {'alt_text_validator.pipelines.CSVOutput': 300, }
@@ -25,26 +26,32 @@ class ATVSpider(CrawlSpider):
 
     def parse_missing_alt_text(self, response: Response) -> InvalidAltTextImage:
         """
-        :param response: a scrapy response object
+        :param response: a scrapy Response object
         :yield: an invalid img
         """
         title = response.css('title::text').get()
         imgs = response.css('#content').xpath('./descendant::img')
-        invalid_imgs = [img for img in imgs if re.fullmatch('\s*', img.attrib['alt'])]
 
-        if invalid_imgs:
-            for img in invalid_imgs:
-                invalid_img_obj = InvalidAltTextImage()
-                invalid_img_obj['origin'] = response.url
-                invalid_img_obj['page_title'] = title
-                invalid_img_obj['src'] = self.assemble_absolute_link(response.url, img.attrib['src'])
-                yield invalid_img_obj
+        invalid_imgs = []
+        for img in imgs:
+            try:
+                if re.fullmatch('\s*', img.attrib['alt']):
+                    invalid_imgs.append(img)
+            except KeyError as e:
+                invalid_imgs.append(img)
+
+        for img in invalid_imgs:
+            invalid_img_obj = InvalidAltTextImage()
+            invalid_img_obj['origin'] = response.url
+            invalid_img_obj['page_title'] = title
+            invalid_img_obj['src'] = self.assemble_absolute_link(response.url, img.attrib['src'])
+            yield invalid_img_obj
 
     def parse_start_url(self, response: Response) -> InvalidAltTextImage:
         """
         Passes the first response to parse_missing_alt_text
 
-        :param response: a scrapy response object
+        :param response: a scrapy Response object
         :yield: an invalid img
         """
         yield from self.parse_missing_alt_text(response)
